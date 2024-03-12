a2ba0e5c5673edd45980d92f0f4ce89ba0c67d62
Santiago Mitidiero <santimiti10@gmail.com>
2024-03-11 22:44:56 -0300

    add colors to theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,28 @@
+import colors from 'tailwindcss/colors';
+
+function flattenColors(nestedColors) {
+  const flatColors = {};
+
+  Object.entries(nestedColors).forEach(([color, values]) => {
+    if (typeof values === 'string') {
+      flatColors[color] = values;
+    } else {
+      Object.entries(values).forEach(([key, value]) => {
+        flatColors[`${color}-${key}`] = value;
+      });
+    }
+  });
+
+  return flatColors;
+}
+
+const flatColors = flattenColors(colors);
+
 export default {
   backgroundColor: {
-
+    ...flatColors,
   },
   color: {
-    
+    ...flatColors,
   }
 }
