c86cfdba9a0b6fb68b3b3bb1131e239a32b4b24a
Santiago Mitidiero <santimiti10@gmail.com>
2024-03-12 19:12:44 -0300

    add tooltip fontsize

--- src/theme.js
+++ src/theme.js
@@ -27,5 +27,13 @@ export default {
   },
   transition: {
     "default": "opacity 300ms",
+  },
+  fontSize: {
+    "sm": "0.875rem",
+    "base": "1rem",
+    "lg": "1.125rem",
+    "xl": "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
   }
 }
